0ceeae4ecc776893d28a7acdd492279475b796d3
Joakim Villo <villokodehode@gmail.com>
2023-10-25 13:06:49 +0200

    *

--- src/components/buttonGallery/buttonGallery.jsx
+++ src/components/buttonGallery/buttonGallery.jsx
@@ -2,14 +2,14 @@ import React, { useState } from 'react';
 import { Button } from './button';
 
 const ButtonGallery = () => {
-    const [active, setActive] = useState(null);
+    const [active, setActive] = useState("Warning");
 
     const handleButtonClick = (buttonName) => {
         setActive(buttonName);
     };
 
     return (
-        <div className="flex flex-col gap-y-3 bg-gradient-to-br from-blue-800 via-slate-800 to-blue-600 p-8">
+        <div className="flex flex-col p-8 gap-y-3 bg-gradient-to-br from-blue-800 via-slate-800 to-blue-600">
             <a href="/">
                 <button
                     className={`bg-cyan-500 min-w-[195px] hover:bg-cyan-700 hover:scale-105 text-white font-bold transition-all py-2 px-4 rounded active:scale-95`}
@@ -31,6 +31,7 @@ const ButtonGallery = () => {
             <Button text="Danger" color="red" handleButtonClick={handleButtonClick} active={active} />
             <Button text="Warning" color="stone" handleButtonClick={handleButtonClick} active={active} />
             <Button text="Custom" color="purple" handleButtonClick={handleButtonClick} active={active} />
+            <Button text="New" color="red" handleButtonClick={handleButtonClick} active={active} />
         </div>
     );
 };
